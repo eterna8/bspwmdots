3d34c051f9a58da9c0b519fa225bb4b2e0bb8fb4
eterna8 <eterna8@proton.me>
2023-09-04 23:26:51 +0300

    installer fix

--- copy.sh
+++ copy.sh
@@ -4,8 +4,10 @@ if [[ $process == y || $process == Y ]]; then
 echo Copying files to ~/.config
 cd config
 cp -r alacritty bspwm rofi polybar sxhkd scripts dunst ~/.config
+cd -
 sh .zshinstall.sh
 elif [[ $process == n|| $process == N ]]; then
 clear
+cd -
 sh .zshinstall.sh
 fi

0c593d28de1027ea2d940ce3e5e3119b0bd220b2
eterna8 <eterna8@proton.me>
2023-09-04 16:08:50 +0300

    installer added

---
+++ archinstaller.sh
@@ -0,0 +1,16 @@
+echo "Welcome to the config&needed apps setup!"
+read -p "Do you want to start installation? (yes/no) " yn
+
+case $yn in
+        yes ) echo starting....;;
+        no ) echo Thanks.;
+                exit;;
+        * ) echo invalid response;
+                exit 1;;
+sudo pacman -S --noconfirm bspwm sxhkd polybar dolphin alacritty dunst rofi qt5ct kvantum  flameshot ttf-font-awesome ttf-cascadia-code zsh kcolorchooser nitrogen
+git clone --depth 1 https://aur.archlinux.org/zsh-theme-powerlevel10k-git && cd
+zsh-theme-powerlevel10k-git && makepkg -si && cd -
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+sh copy.sh
---
+++ copy.sh
@@ -0,0 +1,27 @@
+echo Welcome to copy wizard!
+read -p "Do you want to start installation? (yes/no) " yn
+
+case $yn in
+        yes ) echo starting....;;
+        no ) echo Thanks.;
+                exit;;
+        * ) echo invalid response;
+                exit 1;;
+esac
+echo Copying files to ~/.config
+cd config
+cp -r alacritty bspwm polybar sxhkd scripts dunst
+
+read -p "Do you want to install Zsh configration? (yes/no) " yn
+
+case $yn in 
+	yes ) echo starting....;;
+	no ) echo Thanks.;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
+esac
+
+echo Copying Zsh configrations to Home directory...
+
+cp -r .zshrc .oh-my-zsh .zsh ~
